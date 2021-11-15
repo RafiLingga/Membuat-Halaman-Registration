4e47ca2ab6844a8aee9951b8de36f71dcf5259b1
Rafi Lingga Maulana <90681112+RafiLingga@users.noreply.github.com>
2021-11-15 18:59:36 +0700

    Add files via upload

---
+++ auth.php
@@ -0,0 +1,50 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class auth extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
+    public function index()
+    {
+        $data['title'] = 'Login Page';
+        $this->load->view('templates/auth_header', $data);
+        $this->load->view('auth/login');
+        $this->load->view('templates/auth_footer');
+    }
+
+    public function registration()
+    {
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', ['is_unique' => 'This Email Has Already Registered']);
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[12]|matches[password2]', ['matches' => 'Password Dont Match!', 'min_length' => 'Password To Short!']);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Registration';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registration');
+            $this->load->view('templates/auth_footer');
+        } else {
+            $data = [
+                'name' => $this->input->post('name'),
+                'email' => $this->input->post('email'),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
+                'role_id' => 2,
+                'is_active' => 1,
+                'date_created' => time()
+            ];
+
+            $this->db->insert('user', $data);
+            $this->session->set_flashdata('mesaage', '<div class="alert
+            alert-success" role="alert">Congrulation! Your Account Has Been Created</div');
+            redirect('auth');
+        }
+    }
+}
